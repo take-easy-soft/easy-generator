23f5217176112347184a804637011d166d708dca
wulongcha <wulongcha@aiedu.com>
2020-04-08 08:54:31 +0800

    update

--- main.js
+++ main.js
@@ -6,7 +6,49 @@ let express = require('express');
 let app = express();
 let fs = require('fs');
 
-app.render(controllerTemplate, {}, function (err, str) {
+
+let data = {
+	"name": "valid",
+	"desc": "C端移动端组织相关的接口",
+	"platform": "P",
+	"userType": "B",
+	"package": "com.etm.example",
+	"author": "乌龙茶",
+	"path": "/valid",
+	"apis": [
+		{
+			"name": "validPathVariable",
+			"desc": "校验路径参数",
+			"path": "/path_variable/{name}",
+			"method": "GET",
+			"request": {
+				"pathVar": [
+					{
+						"name": "name",
+						"type": "String",
+						"desc": "名称",
+						"valid": {
+							"type": "NotNull",
+							"message": "名称格式不正确"
+						}
+					}
+				]
+			},
+			"response": {
+				"body": [
+					{
+						"name": "result",
+						"type": "String",
+						"desc": "结果"
+					}
+				]
+			}
+		}
+	]
+
+};
+
+app.render(controllerTemplate, {data:data}, function (err, str) {
 	if (err) {
 		console.error(err);
 		return;
