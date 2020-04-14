0047f2132518c9315669628cd6e597a7b220dc86
cweijan <cweijan@163.com>
2020-04-14 16:00:39 +0800

    增加友好提示

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { Generator } from './core/generator';
 import * as fs from 'fs';
 import { Controller } from './type/controller';
 
-var args = process.argv.slice(2);
+const version = "1.1.3";
+const args = process.argv.slice(2);
 const input = args[0];
 if (input && input.toLowerCase() == "new") {
-    fs.copyFileSync(__dirname+"/../res/template.json",process.cwd()+"/template.json")
+    fs.copyFileSync(__dirname + "/../res/template.json", process.cwd() + "/template.json")
     console.log("create template success!")
 } else if (input) {
 
@@ -18,6 +19,8 @@ if (input && input.toLowerCase() == "new") {
     generator.genrate(JSON.parse(inputData) as Controller)
 
 } else {
+    console.log(`Your etmgen version is ${version}`)
+    console.log('')
     console.log('Usage: etmgen <path>|new')
     console.log('')
     console.log('When You type etmgen new, that will create template in current directory.')
